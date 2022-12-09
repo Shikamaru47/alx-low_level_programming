ade23ef506ceb4a77eb891a44d1c8c83408d5879
Kagiso <kagisorakgoale34@gmail.com>
2022-12-09 05:48:51 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char n;
+	char m;
+
+        n = 'a';
+	m = 'A';
+        while
+                (n <= 'z') {
+                        putchar(n);
+              		n++;
+		}
+	while
+                (m <= 'Z') {
+                        putchar(m);
+                        m++;
+
+                }
+putchar('\n');
+return (0);
+}
