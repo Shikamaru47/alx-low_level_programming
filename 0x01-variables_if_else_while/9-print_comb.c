e41c4fd2a6ae8e95c33f7da494966ad0685668c2
Kagiso <kagisorakgoale34@gmail.com>
2022-12-09 09:07:59 -0800

    print commb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,18 +10,17 @@
 int main(void)
 
 {
-        int num = '0';
-
-        while (num <= '9')
-        {
-                putchar(num);
-                if (num != '9')
-                {
-                        putchar(',');
-                        putchar(' ');
-                }
-                num++;
-        }
-        putchar('\n');
-        return (0);
+int num = '0';
+while (num <= '9')
+{
+putchar(num);
+if (num != '9')
+{
+putchar(',');
+putchar(' ');
+}
+num++;
+}
+putchar('\n');
+return (0);
 }
