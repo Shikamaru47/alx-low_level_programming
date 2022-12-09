a8755c4fc67aa1775fe1f8f4745319753b2af414
Kagiso <kagisorakgoale34@gmail.com>
2022-12-09 07:18:56 -0800

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	char a;
+	int b;
+
+	a = 'a';
+	b = 0;
+	while
+		(b < 10) {
+			putchar(b = '0');
+			b++;
+		}
+	while
+		(a <= 'f') {
+			putchar(a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
