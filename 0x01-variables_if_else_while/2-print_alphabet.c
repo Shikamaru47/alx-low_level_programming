c0ed456a34004977724ffc20029a4a1c8288897f
Kagiso <kagisorakgoale34@gmail.com>
2022-12-09 04:50:30 -0800

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n;
+
+	n = 'a';
+	while
+		(n <= 'z'){
+			putchar(n);
+			n++;
+		}
+putchar('\n');
+return (0);
+}
