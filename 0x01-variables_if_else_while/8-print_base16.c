e2e58a2a988846b6df4a2bb135dff7dd267bf8a9
Kagiso <kagisorakgoale34@gmail.com>
2022-12-09 07:54:13 -0800

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,15 +9,12 @@
 
 int main(void)
 {
-	char la;
-	int num;
-
-	for (num = 0; num < 10; num++)
-			putchar((num % 10) + '0');
-
-	for (la = 'a'; la <= 'f'; la++)
-			putchar(la);
-	
-	putchar('\n');
-	return (0);
+char la;
+int num;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+for (la = 'a'; la <= 'f'; la++)
+putchar(la);
+putchar('\n');
+return (0);
 }
