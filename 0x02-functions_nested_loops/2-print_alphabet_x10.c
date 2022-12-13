dceda2e7342d005cf26f6fe30cdc348f7eee6a2d
Kagiso <kagisorakgoale34@gmail.com>
2022-12-13 04:44:41 -0800

    fix2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)i
+{
+char c = 'a';
+int d;
+
+while (c <= 'z')
+{
+	for (d = 0; d <= 10; d++)
+	{
+
+_putchar(c);
+c++;
+}
+}
+_putchar('\n');
+}
+~
+
