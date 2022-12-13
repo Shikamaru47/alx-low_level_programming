5723d88bf5e066f429c4e2e87c8aec4fb9fe0482
Kagiso <kagisorakgoale34@gmail.com>
2022-12-13 05:06:55 -0800

    fix2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,18 +6,19 @@
  * Return: Always 0.
  */
 
-void print_alphabet_x10(void)
 {
-char c = 'a';
-int d;
-d = 0;
-do (c <= 'z')
+char c;
+int n = 0;
+
+	while (n <= 9)
+{
+	for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
-c++;
 }
-while (d <= 10)
-{
+
 _putchar('\n');
+
+n++;
 }
-}
+}i
