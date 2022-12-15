790c0326eefebbaa8a7b11d3119642a651b2a9bf
Kagiso <kagisorakgoale34@gmail.com>
2022-12-15 06:43:43 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints line
+ * @n: param
+ */
+
+void print_line(int n)
+{
+	int m = 1;
+
+	while (m <=  n)
+	{
+		_putchar(95);
+		m++;
+	}
+	_putchar('\n');
+}
