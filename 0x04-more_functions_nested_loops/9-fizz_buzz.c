7517048a4cc4f4b9d7ba05b9eb3dbeb1665d25a8
Kagiso <kagisorakgoale34@gmail.com>
2022-12-15 07:41:38 -0800

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - fizbuzz
+ * Return: 0
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = 1; k < 100; k++)
+	{
+		if (k % 3 == 0 && k % 5 == 0)
+			printf("FizzBuzz ");
+		else if (k % 3 == 0)
+			printf("Fizz ");
+		else if (k % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", k);
+	}
+	printf("Buzz\n");
+	return (0);
+}
