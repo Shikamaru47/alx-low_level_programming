16389e1f80fec17fcf9514cb3f8bffd74603e2da
Kagiso <kagisorakgoale34@gmail.com>
2022-12-20 03:42:53 -0800

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* puts2 - function that prints a string, followed by a new line, to stdout
+* @str: pointer value
+* Return: Always 0 (Success)
+*/
+
+void puts2(char *str)
+{
+	int k;
+
+	k = 0;
+
+	while (str[k] != '\0')
+	{
+		if (k % 2 == 0)
+			_putchar(str[k]);
+		 k++;
+	}
+
+	_putchar('\n');
+}
