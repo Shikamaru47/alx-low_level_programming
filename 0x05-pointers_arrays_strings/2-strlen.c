9f01ed2631db608df282cfa78630f77265d14f3f
Kagiso <kagisorakgoale34@gmail.com>
2022-12-20 03:34:29 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strlen - a function to print the length of a string
+* @s: string to be supplied
+* Return: Always 0 (Success)
+*/
+
+int _strlen(char *s)
+{
+	int index = 0;
+
+	while (s[index] != '\0')
+	{
+		index++;
+	}
+	return (index);
+}
