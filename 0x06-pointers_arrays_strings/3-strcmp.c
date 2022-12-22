6c933dcc7f56522595591a8614dfd784ce1eb1d0
Kagiso <kagisorakgoale34@gmail.com>
2022-12-22 10:04:57 -0800

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,15 +9,18 @@
  * Value greater than 0 if string is greater then the other
  * 0 if strings are equal
  */
+
 int _strcmp(char *s1, char *s2)
 {
-	int compare, counter;
+	int counter, comVal;
 
 	counter = 0;
-	while (s1[counter] == s2[counter] && s1 != '\0')
+
+	while (s1[counter] == s2[counter] && s1[counter] != '\0')
 	{
 		counter++;
 	}
-	compare = s1[counter] - s2[counter];
-	return (compare);
+
+	comVal = s1[counter] - s2[counter];
+	return (comVal);
 }
