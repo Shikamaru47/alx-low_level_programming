4211607a7781e75bfa84b54d3590ec1cd0c675cb
Kagiso <kagisorakgoale34@gmail.com>
2023-01-03 04:20:30 -0800

    memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -18,5 +18,6 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+void *_memset(void *s, int b, size_t n);
 
 #endif
