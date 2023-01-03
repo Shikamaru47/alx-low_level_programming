5761599925dce9091048578247272de865e918a0
Kagiso <kagisorakgoale34@gmail.com>
2023-01-03 04:25:10 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,21 +2,22 @@
 
 /**
  * _memset() function fills the first n bytes of the memory area
- * pointed to by @s with the constant byte @b
+ * pointed to by @s with the constant byte @c
  * @s: A pointer to the memory are to be filled
- * @b: The character to fill the memory area with
+ * @c: The character to fill the memory area with
  * @n: The number ofbytes to be filled.
  * description_memset: over there
  *
  * Returns: a pointer to the memory area s
  */
 
-void *_memset(void *s, int b, size_t n)
+void *_memset(void *s, int c, size_t n)
 {
 	unsigned int index;
-	unsigned char *memory = s, value = b;
+	unsigned char *memory = s, value = c;
 
 	for (index = 0; index < n; index++)
 		memory[index] = value;
+
 	return (memory);
 }
