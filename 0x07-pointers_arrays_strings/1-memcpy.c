676e125ef880f813e9178f12788d3f61bba24837
Kagiso <kagisorakgoale34@gmail.com>
2023-01-03 04:54:48 -0800

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,20 @@
 #include "main.h"
 
 /**
- *
+ * _memset - fills memory with a constant byte
+ * @dest: source string
+ * @src: the constant byte of filling
+ * @n: length of buffer
+ * Return: new string.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i + 0;
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
